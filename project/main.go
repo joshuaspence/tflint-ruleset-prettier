f31b5814c7af99752e55c3f291c8f6eee5a256c9
Josh Spence <josh@spence.com.au>
2026-06-19 00:00:11 +1000

    Fix version

--- project/main.go
+++ project/main.go
@@ -2,10 +2,10 @@ package project
 
 import "fmt"
 
-// Version is ruleset version
-const Version string = "0.2.0"
+var (
+  Version = "dev"
+)
 
-// ReferenceLink returns the rule reference link
 func ReferenceLink(name string) string {
 	return fmt.Sprintf("https://github.com/joshuaspence/tflint-ruleset-prettier/blob/v%s/docs/rules/%s.md", Version, name)
 }
